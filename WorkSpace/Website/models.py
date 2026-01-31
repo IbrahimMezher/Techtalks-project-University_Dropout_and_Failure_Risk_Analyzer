187c5473087ba3c32ac14b56e5590cf0e5d4ad6a
IbrahimMezher <ibrahim.mezher1958@gmail.com>
2026-01-31 15:57:12 +0200

    finished intro/signup/login pages with a db still want to add one thing where it reads the flash and displays message under each <input>

---
+++ WorkSpace/Website/models.py
@@ -0,0 +1,10 @@
+from flask_login import UserMixin
+from . import db
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(30))
+    last_name = db.Column(db.String(30))
+    email = db.Column(db.String(70), unique=True)
+    password = db.Column(db.String(50))
