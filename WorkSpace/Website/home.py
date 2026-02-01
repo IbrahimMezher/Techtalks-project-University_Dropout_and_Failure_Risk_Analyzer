a45acdf7011e9222887d2658dd4f4403f24c6cc2
IbrahimMezher <ibrahim.mezher1958@gmail.com>
2026-02-01 17:37:35 +0200

    test
    cleanup : remove vscode/pychache and update pages

--- WorkSpace/Website/home.py
+++ WorkSpace/Website/home.py
@@ -1,1 +1,25 @@
+<<<<<<< HEAD
 
+=======
+from flask import Blueprint, render_template, request, url_for
+from flask_login import login_required
+import smtplib
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/home')
+@login_required
+def home():
+    return render_template("homepage.html")
+
+
+@views.route('/', methods=['GET', 'POST'])
+def intro():
+    return render_template("introductorypage.html")
+
+
+@views.route('/Contact_us', methods=['GET', 'POST'])
+def contact():
+    return render_template("introductorypage.html")
+>>>>>>> ffbc0ce (test)
