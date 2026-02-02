5fdcf7fbf3a3d0e83da4bf7d66091da35012f070
IbrahimMezher <ibrahim.mezher1958@gmail.com>
2026-02-02 11:46:13 +0200

    Update home.py

--- WorkSpace/Website/home.py
+++ WorkSpace/Website/home.py
@@ -1,15 +1,1 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
 
-views = Blueprint('views', __name__)
-
-
-@views.route('/home')
-@login_required
-def home():
-    return render_template("homepage.html")
-
-
-@views.route('/', methods=['GET', 'POST'])
-def intro():
-    return render_template("introductorypage.html")
